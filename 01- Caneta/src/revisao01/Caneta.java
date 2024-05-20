7e89743df33372a02ac68aa9d1fc823d2f54707a
Willian Ferreira <willian.ferreira.gomes96@gmail.com>
2024-05-20 17:41:19 -0300

    Revisão Caneta

---
+++ 01- Caneta/src/revisao01/Caneta.java
@@ -0,0 +1,19 @@
+package revisao01;
+
+public class Caneta {
+	//Atributos
+	String modelo;
+	String cor;
+	double ponta;
+	int  carga;
+	boolean tampada;
+	
+	//Metodos	
+	void rabiscar() {
+		
+	}
+	
+	void tampar() {
+		
+	}
+}
---
+++ 01- Caneta/src/revisao01/Main.java
@@ -0,0 +1,9 @@
+package revisao01;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
